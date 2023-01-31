b773cdacdd16b915d3d4f3bd3722a7d4d9d38e7f
ranajoy <ranajoybhattacharjee19@gmail.com>
2023-02-01 01:02:52 +0530

    base files

---
+++ zwigato/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zwigato project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zwigato.settings')
+
+application = get_wsgi_application()
