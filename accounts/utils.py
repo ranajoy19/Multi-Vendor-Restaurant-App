3163620ad1e25784821fa3f76ef7a62791c4790e
ranajoy <ranajoybhattacharjee19@gmail.com>
2023-05-20 01:52:10 +0530

    email send and forget password

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,13 +1,31 @@
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.sites.shortcuts import get_current_site
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
 
-
+from django.template.loader import render_to_string
 
 def detectUser(user):
     if user.role == 1:
-        redirectUrl = 'vendorDashboard'
-        return redirectUrl
+        return 'vendorDashboard'
     elif user.role == 2:
-        redirectUrl = 'custDashboard'
-        return redirectUrl
-    elif user.role == None and user.is_superadmin:
-        redirectUrl = '/admin'
-        return redirectUrl
+        return 'custDashboard'
+    elif user.role is None and user.is_superadmin:
+        return '/admin'
+    
+
+def send_verification_email(request, user, mail_subject, email_template):
+    cuttent_site = get_current_site(request)
+    from_email = settings.EMAIL_HOST
+    message = render_to_string(email_template,{
+        'user':user,
+        'domain':cuttent_site,
+        'uid' :urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.content_subtype = "html"
+    mail.send()
